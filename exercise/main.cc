66bc96b9d4aeb304d0831bcbe234a8d2db3ea06d
SenseiEnrique <migonzo@web.de>
2022-04-28 21:01:46 +0200

    28.04.2022

--- exercise/main.cc
+++ exercise/main.cc
@@ -94,25 +94,59 @@ void ply_example()
 	write_ply_mesh(os, vertices, faces);
 }
 
-void get_num_pixels(const char* path, const char* filename_base, const char* extension, unsigned int& image_height, unsigned int& image_width, unsigned int& image_channels, unsigned int& num_pixels) {
+image_b get_num_pixels(const char* path, const char* filename_base, const char* extension, unsigned int& image_height, unsigned int& image_width, unsigned int& image_channels, unsigned int& num_pixels) {
 	std::string path_str(path);
 	std::string base_str(filename_base);
 	std::string ext_str(extension);
 	std::string file_str = path_str + "\\" + base_str + "0" + ext_str;
 	image_b image = image_io::load(file_str.data());
-	image_height = image.height();
-	image_width = image.width();
-	image_channels = image.channels();
-	num_pixels = image_height * image_width;
+	
+
+	return image;
 }
 
-void load_images(const char* path, const char* filename_base, const char* extension, const unsigned int num_pixels, const unsigned int num_times, unsigned int * pixels) {
+std::vector<image_b> load_images(const char* path, const char* filename_base, std::vector<float>& times, unsigned int& num_images) {
+
 	std::string path_str(path);
 	std::string base_str(filename_base);
-	std::string ext_str(extension);
-	for (unsigned int i = 0; i < num_times; ++i) {
-		const std::string file_str = path_str + "\\" + base_str + std::to_string(i) + ext_str;
-		image_b image = image_io::load(file_str.data());
+	std::string hdrgen = path_str + "\\" + base_str;
+	std::ifstream data;
+	data.open(hdrgen);
+
+	std::vector<image_b> images;
+	while (!data.eof()) {
+		std::string filename;
+		data >> filename;
+		//std::cout << filename;
+		const std::string file_str = path_str + "\\" + filename;
+		std::cout << file_str << "\n";
+		images.push_back(image_io::load(file_str.data()));
+		float time;
+		data >> time;
+		times.push_back(time);
+		++num_images;
+	}
+	data.close();
+	return images;
+}
+
+void calculate_weights(const image_b image, const unsigned int num_pixels, float* weights) {
+	for (int i = 0; i < num_pixels; ++i) {
+		const float difference = static_cast<float>(image.at(i, 0)) - 127.5;
+		const float exponent = -4 * ((difference * difference) / (127.5 * 127.5));
+		weights[i] = std::exp(exponent);
+	}
+}
+
+void calculate_weights_all_images(const std::vector<image_b> images, const unsigned int num_images, const unsigned int num_pixels, float* weights) {
+	for (unsigned int i = 0; i < num_images; ++i) {
+		calculate_weights(images[i], num_pixels, weights + i * num_pixels);
+	}
+}
+
+void calculate_initial_i_m(float* i_m) {
+	for (unsigned int i = 0; i < 255; ++i) {
+		i_m[i] = i * (1.0 / 128.0);
 	}
 }
 
@@ -145,6 +179,23 @@ void calculate_i_m_values(const unsigned int* cardinalities, std::tuple<unsigned
 
 }
 
+void calculate_irradiances(const float* weights, const float* times, const float* i_m, std::vector<image_b> images, const unsigned int num_images, const unsigned int num_pixels, float* irradiance) {
+
+	for (unsigned int h = 0; h < num_pixels; ++h) {
+		float sum_1 = 0;
+		float sum_2 = 0;
+		for (unsigned int i = 0; i < num_images; ++i) {
+			const unsigned int index = h + i * num_pixels;
+			const unsigned int pixel_value = images[i].at(h);
+			sum_1 += weights[index] * times[i] * i_m[pixel_value];
+
+			sum_2 += weights[index] * times[i] * times[i];
+		}
+
+		irradiance[h] = sum_1 / sum_2;
+	}
+}
+
 // calculates the objective function for unknown response function
 float calculate_objective_function(const float* weights, const float* times, const float* i_values, const float* irradiances, const unsigned int num_images, const unsigned int num_pixels) {
 
@@ -168,20 +219,33 @@ float calculate_objective_function(const float* weights, const float* times, con
 
 int main(int argc, const char **argv)
 {
-	if (argc != 4) throw std::runtime_error("Invalid arguments");
+	if (argc != 3) throw std::runtime_error("Invalid arguments");
 	const char* path = argv[1];
 	const char* filename_base = argv[2];
-	const char* extension = argv[3];
-	unsigned int image_height, image_width, image_channels, num_pixels;
-	get_num_pixels(path, filename_base, extension, image_height, image_width, image_channels, num_pixels);
+
+	std::vector<float> times;
+	unsigned int num_images = 0;
+
+	std::vector<image_b> images = load_images(path, filename_base, times, num_images);
+
+	const unsigned int image_height = images[0].height();
+	const unsigned int image_width = images[0].width();
+	const unsigned int image_channels = images[0].channels();
+	const unsigned int num_pixels = image_height * image_width;
 
 	std::cout << "height " << image_height << " width " << image_width << " channels " << image_channels << " pixels per image: " << num_pixels << "\n";
-		 
-	const unsigned int num_times = 14;
 
-	unsigned int* pixels = (unsigned int*) malloc(num_times * num_pixels * sizeof(unsigned int));
+	std::vector<float> weights(num_pixels * num_images);
 
-	load_images(path, filename_base, extension, num_pixels, num_times, pixels);
+	calculate_weights_all_images(images, num_images, num_pixels, weights.data());
+
+	float i_m[255];
+
+	calculate_initial_i_m(i_m);
+
+	std::vector<float> irradiances(num_pixels);
+
+	calculate_irradiances(weights.data(), times.data(), i_m, images, num_images, num_pixels, irradiances.data());
 
 	//example(argv[1]);
 
@@ -192,4 +256,21 @@ int main(int argc, const char **argv)
 	//ply_example();
 
 	return EXIT_SUCCESS;
+
+	/*
+	image_b image = image_io::load("C:\\Users\\migon\\Documents\\Capturing Reality\\Exercise 1\\HDR Sequence\\max0.ppm");
+	for (unsigned int i = 0; i < image.width(); ++i) {
+		for (unsigned int j = 0; j < image.height(); ++j) {
+			unsigned char* ptr = image.ptr2d(i, j, 0);
+			*ptr = 0;
+		}
+	}
+	for (unsigned int i = 0; i < image.width(); ++i) {
+		for (unsigned int j = 0; j < image.height(); ++j) {
+			unsigned char* ptr = image.ptr2d(i, j, 1);
+			*ptr = 0;
+		}
+	}
+	image_io::save_png(image, "C:\\Users\\migon\\Documents\\Capturing Reality\\test.png");
+	*/
 }
