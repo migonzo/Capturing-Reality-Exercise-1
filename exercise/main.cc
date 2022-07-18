2050ce3a0b535b9baf7947c088d224f66f046312
SenseiEnrique <migonzo@web.de>
2022-07-18 19:32:57 +0200

    Reading depth images and camera info + conversion of pixel coordinates to world coordinates

--- exercise/main.cc
+++ exercise/main.cc
@@ -7,104 +7,108 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <fstream>
+#include <iomanip>
 
-// #define FITTING_EXAMPLE
+struct camera_info {
+	double focal_length;
+	double pixel_aspect;
+	Eigen::Vector2d principal_point;
+	Eigen::Matrix3d rotation;
+	Eigen::Vector3d translation;
+};
 
-#ifdef FITTING_EXAMPLE
-#include "ceres/ceres.h"
-#endif
+std::vector<camera_info> load_camera_infos(const char* path, const unsigned int num) {
+	std::vector<camera_info> infos(num);
+	for (int i = 0; i < num; ++i) {
+		std::stringstream stream;
+		stream << path << "/" << "view_" << std::setw(4) << std::setfill('0') << i << "-cam.txt";
+		std::string file_str = stream.str();
+		std::ifstream data;
+		data.open(file_str);
+		if (!data.is_open()) {
+			std::cout << "Could not open " << file_str << "\n";
+			throw std::runtime_error("Could not open camera info file");
+		}
 
-void example(const char *filename)
-{
-	std::cout << "Running example code..." << std::endl;
+		float focal, aspect, p_point_0, p_point_1, rot_00, rot_01, rot_02, rot_10, rot_11, rot_12, rot_20, rot_21, rot_22, trans_0, trans_1, trans_2;
+		if (!(data >> focal >> aspect
+			>> p_point_0 >> p_point_1
+			>> rot_00 >> rot_01 >> rot_02
+			>> rot_10 >> rot_11 >> rot_12
+			>> rot_20 >> rot_21 >> rot_22
+			>> trans_0 >> trans_1 >> trans_2
+			)) 
+		{
+			std::cout << "Invalid camera info file\n";
+			throw std::runtime_error("Invalid camera info file");
+		}
 
-	image_b img_b = image_io::load(filename);
+		Eigen::Vector2d p_point(p_point_0, p_point_1);
+		Eigen::Matrix3d rot;
+		rot << rot_00, rot_01, rot_02, rot_10, rot_11, rot_12, rot_20, rot_21, rot_22;
+		Eigen::Vector3d trans(trans_0, trans_1, trans_2);
 
-	float r = 50.f;
-	if (img_b.width() < r + r + 10 || img_b.height() < r + r + 10) throw std::runtime_error("Image too small");
+		camera_info info{ focal, aspect, p_point, rot, trans };
 
-	for (int y = -r; y < r; ++y) {
-		for (int x = -r; x < r; ++x) {
-			Eigen::Vector2d p(x, y);
-			Eigen::Vector2d pa = p + Eigen::Vector2d(r + 10, r + 10);
-			if (p.norm() <= r) {
-				img_b.at_v(pa, 0) = 255;
-				img_b.at_v(pa, 1) = 0;
-				img_b.at_v(pa, 2) = 0;
+		infos.push_back(info);
+	}
+
+	return infos;
+}
+
+std::vector<image_f> load_depth_images(const char* path, const unsigned int num) {
+	std::vector<image_f> depth_images(num);
+	for (int i = 0; i < num; ++i) {
+		std::stringstream stream;
+		stream << path << "/" << "view_" << std::setw(4) << std::setfill('0') << i << "-L2.pfm";
+		std::string file_str = stream.str();
+		
+		depth_images.push_back(image_io::load<float>(file_str.c_str()));
+	}
+
+	return depth_images;
+}
+
+inline Eigen::Vector3d to_world_coordinates(const Eigen::Vector2d x_pixel, const camera_info info, const double depth) {
+	const Eigen::Vector2d x_dist = (x_pixel - info.principal_point) / info.focal_length;
+	const Eigen::Vector3d x_camera = x_dist.normalized() * depth;
+	return info.rotation.transpose() * (x_camera - info.translation);
+}
+
+std::vector<Eigen::Vector3d> create_3d_points(image_f const * images, camera_info const * infos, const unsigned int num_images, const unsigned int num_points) {
+	std::vector<Eigen::Vector3d> points(num_points);
+	for (int i = 0; i < num_images; ++i) {
+		const image_f image = images[i];
+		const camera_info info = infos[i];
+
+		for (int j = 0; j < image.width(); ++j) {
+			for (int h = 0; h < image.height(); ++h) {
+				const double depth = image.at2d(j, h);
+				const Eigen::Vector2d x_pixel(j, h);
+				points.push_back(to_world_coordinates(x_pixel, info, depth));
 			}
 		}
 	}
 
-	image_f img_f = image_manip::grayscale(img_b);
-
-	image_io::save(img_b, "out.jpg");
-	image_io::save(img_b, "out.png");
-	image_io::save(img_f, "out.pfm");
+	return points;
 }
 
-#ifdef FITTING_EXAMPLE
-// adopted from: https://ceres-solver.googlesource.com/ceres-solver/+/master/examples/helloworld.cc
-struct ExampleCostFunctor {
-	template <typename T>
-	bool operator()(const T *const x, T *residual) const
-	{
-		residual[0] = 10.0 - x[0];
-		return true;
-	}
-
-	static ceres::CostFunction *Create()
-	{
-		return new ceres::AutoDiffCostFunction<ExampleCostFunctor, 1, 1>(new ExampleCostFunctor);
-	}
-};
-
-void fitting_example()
-{
-	std::cout << "Running fitting example code..." << std::endl;
-
-	double x = 0.5;
-	const double initial_x = x;
-
-	// Build the problem.
-	ceres::Problem problem;
-
-	// Set up the only cost function (also known as residual). This uses
-	// auto-differentiation to obtain the derivative (jacobian).
-	ceres::CostFunction *cost_function = ExampleCostFunctor::Create();
-	problem.AddResidualBlock(cost_function, NULL, &x);
-
-	// Run the solver!
-	ceres::Solver::Options options;
-	options.linear_solver_type = ceres::DENSE_QR;
-	options.minimizer_progress_to_stdout = true;
-	ceres::Solver::Summary summary;
-	ceres::Solve(options, &problem, &summary);
-
-	std::cout << summary.BriefReport() << std::endl;
-	std::cout << "x: " << initial_x << " -> " << x << std::endl;
+inline Eigen::Vector3d rotate_to_lcs(const Eigen::Vector3d x, const Eigen::Matrix3d rot, const Eigen::Vector3d pos) {
+	return rot * (x - pos);
 }
-#endif
 
-int ply_example()
-{
-	std::vector<Vertex> vertices = { { 0, 0, 0, 1, 0, 0, 255, 0, 0 }, { 0, 1, 0, 1, 0, 0, 0, 255, 0 }, { 1, 0, 0, 1, 0, 0, 0, 0, 255 } };
-	std::vector<Face> faces = { { 0, 1, 2 } };
+inline double basis_f(const Eigen::Vector3d x) {
 
-	std::ofstream os("out.ply", std::ios_base::binary);
-	write_ply_mesh(os, vertices, faces);
+}
+
+inline double implicit_F(const std::vector<Eigen::Vector3d>& xs) {
+
 }
 
 int main(int argc, const char **argv)
 {
-	if (argc != 2) throw std::runtime_error("Invalid arguments");
-
-	example(argv[1]);
-
-#ifdef FITTING_EXAMPLE
-	fitting_example();
-#endif
-
-	ply_example();
+	const char* path = argv[1];
+	const std::vector<camera_info> infos = load_camera_infos(path);
 
 	return EXIT_SUCCESS;
 }
