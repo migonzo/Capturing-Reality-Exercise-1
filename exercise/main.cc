69dd8ac00c687fe1e431ea1cce366c8849140577
SenseiEnrique <migonzo@web.de>
2022-07-19 23:56:02 +0200

    More stuff up to voxel grid

--- exercise/main.cc
+++ exercise/main.cc
@@ -9,6 +9,8 @@
 #include <fstream>
 #include <iomanip>
 
+#define SIGMA 1.0
+
 struct camera_info {
 	double focal_length;
 	double pixel_aspect;
@@ -18,7 +20,8 @@ struct camera_info {
 };
 
 std::vector<camera_info> load_camera_infos(const char* path, const unsigned int num) {
-	std::vector<camera_info> infos(num);
+	std::vector<camera_info> infos;
+	infos.reserve(num);
 	for (int i = 0; i < num; ++i) {
 		std::stringstream stream;
 		stream << path << "/" << "view_" << std::setw(4) << std::setfill('0') << i << "-cam.txt";
@@ -51,13 +54,16 @@ std::vector<camera_info> load_camera_infos(const char* path, const unsigned int 
 		camera_info info{ focal, aspect, p_point, rot, trans };
 
 		infos.push_back(info);
+
+		std::cout << focal << " " << aspect << "\n" << p_point << rot << trans << "\n";
 	}
 
 	return infos;
 }
 
 std::vector<image_f> load_depth_images(const char* path, const unsigned int num) {
-	std::vector<image_f> depth_images(num);
+	std::vector<image_f> depth_images;
+	depth_images.reserve(num);
 	for (int i = 0; i < num; ++i) {
 		std::stringstream stream;
 		stream << path << "/" << "view_" << std::setw(4) << std::setfill('0') << i << "-depth-L2.pfm";
@@ -71,9 +77,12 @@ std::vector<image_f> load_depth_images(const char* path, const unsigned int num)
 
 inline Eigen::Vector3d to_world_coordinates(const Eigen::Vector2d x_pixel, const camera_info info, const double depth) {
 	const Eigen::Vector2d x_dist = (x_pixel - info.principal_point) / info.focal_length;
-	//const Eigen::Vector3d x_camera = x_dist.normalized() * depth;
+	Eigen::Vector3d x_dist_3d(x_dist(0), x_dist(1), 1.0);
+	x_dist_3d.normalize();
+	//std::cout << info.focal_length << "\n";
+	const Eigen::Vector3d x_camera = x_dist_3d * depth;
 	// maybe wrong
-	const Eigen::Vector3d x_camera(x_dist(0) * depth, x_dist(1) * depth, depth);
+	//const Eigen::Vector3d x_camera(x_dist(0) * depth, x_dist(1) * depth, depth);
 	return info.rotation.transpose() * (x_camera - info.translation);
 }
 
@@ -84,8 +93,10 @@ inline Eigen::Vector3d get_normal(const Eigen::Vector3d x_1, const Eigen::Vector
 }
 
 std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>> create_3d_points(image_f const * images, camera_info const * infos, const unsigned int num_images, const unsigned int num_points) {
-	std::vector<Eigen::Vector3d> points(num_points);
-	std::vector<Eigen::Vector3d> normals(num_points);
+	std::vector<Eigen::Vector3d> points;
+	points.reserve(num_points);
+	std::vector<Eigen::Vector3d> normals;
+	normals.reserve(num_points);
 	for (int i = 0; i < num_images; ++i) {
 		const image_f image = images[i];
 		const camera_info info = infos[i];
@@ -97,6 +108,8 @@ std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>> create_3d_
 				points.push_back(to_world_coordinates(x_pixel, info, depth));
 			}
 		}
+
+		std::cout << "Done with points for image " << i << "\n";
 
 		for (int j = 0; j < image.width(); ++j) {
 			for (int h = 0; h < image.height(); ++h) {
@@ -120,7 +133,7 @@ std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>> create_3d_
 				else {
 					y1 = points[j + h * image.width()];
 				}
-				if (h < image.width() - 1) {
+				if (h < image.height() - 1) {
 					y2 = points[j + (h + 1) * image.width()];
 				}
 				else {
@@ -130,6 +143,8 @@ std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>> create_3d_
 				get_normal(x1, x2, y1, y2);
 			}
 		}
+
+		std::cout << "Done with image " << i << "\n";
 	}
 
 	return std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>>(points, normals);
@@ -150,31 +165,95 @@ inline Eigen::Matrix3d get_rotation_matrix(const Eigen::Vector3d normal) {
 	return rotation;
 }
 
+inline std::vector<Eigen::Matrix3d> get_rotation_matrices(const std::vector<Eigen::Vector3d> normals) {
+	std::vector<Eigen::Matrix3d> rotation_matrices;
+	rotation_matrices.reserve(normals.size());
+	for (const auto& normal : normals) {
+		rotation_matrices.push_back(get_rotation_matrix(normal));
+	}
+
+	return rotation_matrices;
+}
+
 inline Eigen::Vector3d rotate_to_lcs(const Eigen::Vector3d x, const Eigen::Matrix3d rot, const Eigen::Vector3d pos) {
 	return rot * (x - pos);
 }
 
 inline double basis_f(const Eigen::Vector3d x) {
+	const double first = x(0) / (std::pow(SIGMA, 4) * 2 * EIGEN_PI);
+	const double second = std::exp(-(1.0 / (2 * SIGMA * SIGMA)) * x.squaredNorm());
 
+	return first * second;
 }
 
-inline double implicit_F(const std::vector<Eigen::Vector3d>& xs) {
+inline double implicit_F(const Eigen::Vector3d x, const std::vector<Eigen::Matrix3d>& rotations, const std::vector<Eigen::Vector3d>& points) {
+	double result = 0.0;
+	for (int i = 0; i < rotations.size(); ++i) {
+		const Eigen::Vector3d x_i = rotate_to_lcs(x, rotations[i], points[i]);
+		result += basis_f(x_i);
+	}
+}
 
+std::vector<double> iterate_voxels(
+	const std::vector<Eigen::Vector3d> points, 
+	const std::vector<Eigen::Matrix3d> rotations, 
+	const double min_x, const double min_y, const double min_z,
+	const double max_x, const double max_y, const double max_z,
+	const int dim_x, const int dim_y, const int dim_z) 
+{
+	std::vector<double> result;
+	result.reserve(dim_x * dim_y * dim_z);
+
+	const double step_x = (max_x - min_x) / static_cast<double>(dim_x);
+	const double step_y = (max_y - min_y) / static_cast<double>(dim_y);
+	const double step_z = (max_z - min_z) / static_cast<double>(dim_z);
+
+	const double start_x = min_x + step_x / 2.0;
+	const double start_y = min_y + step_y / 2.0;
+	const double start_z = min_z + step_z / 2.0;
+
+	for (int i = 0; i < dim_x; ++i) {
+		for (int j = 0; j < dim_y; ++j) {
+			for (int k = 0; k < dim_z; ++k) {
+				const Eigen::Vector3d x(start_x + i * step_x, start_y + j * step_y, start_z + k * step_z);
+				result.push_back(implicit_F(x, rotations, points));
+			}
+		}
+	}
+
+	return result;
 }
 
 int main(int argc, const char **argv)
 {
-	Eigen::Vector3d normal(0.0, 0.0, 1.0);
-	Eigen::Matrix3d matrix = get_rotation_matrix(normal);
-	const Eigen::Vector3d result = matrix * normal;
-	std::cout << result << "\n";
-
 	const char* path = argv[1];
-	const std::vector<camera_info> infos = load_camera_infos(path, 14);
-	const std::vector<image_f> images = load_depth_images(path, 14);
+	const std::vector<camera_info> infos = load_camera_infos(path, 1);
+	const std::vector<image_f> images = load_depth_images(path, 1);
+	std::cout << "Loaded depth images\n";
 	const int img_size = images[0].size();
 
-	//const std::vector<Eigen::Vector3d> points = create_3d_points(images.data(), infos.data(), 14, 14 * img_size);
+	const std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3d>> result = create_3d_points(images.data(), infos.data(), 1, 1 * img_size);
+	std::cout << "Created points and normals\n";
+	const std::vector<Eigen::Vector3d> points = result.first;
+	double min_x, min_y, min_z;
+	min_x = min_y = min_z = 1000000000.0;
+	double max_x, max_y, max_z;
+	max_x = max_y = max_z = -1000000000.0;
+	for (int i = 0; i < points.size(); ++i) {
+		const Eigen::Vector3d test = points[i];
+		min_x = test(0) < min_x ? test(0) : min_x;
+		min_y = test(1) < min_y ? test(1) : min_y;
+		min_z = test(2) < min_z ? test(2) : min_z;
+		max_x = test(0) > max_x ? test(0) : max_x;
+		max_y = test(1) > max_y ? test(1) : max_y;
+		max_z = test(2) > max_z ? test(2) : max_z;
+	}
+	std::cout << min_x << " " << min_y << " " << min_z << "\n" << max_x << " " << max_y << " " << max_z << "\n";
+
+	const std::vector<Eigen::Vector3d> normals = result.second;
+
+	const std::vector<Eigen::Matrix3d> matrices = get_rotation_matrices(normals);
+	const std::vector<double> voxel_grid = iterate_voxels(points, matrices, min_x, min_y, min_z, max_x, max_y, max_z, 100, 100, 100);
 
 	return EXIT_SUCCESS;
 }
